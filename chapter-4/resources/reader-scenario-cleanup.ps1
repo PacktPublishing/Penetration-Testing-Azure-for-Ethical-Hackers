3ee9fc531549e88296310b9a868461ef097da1df
David Okeyode <asegunlolu@hotmail.com>
2021-01-30 01:28:02 +0000

    modified text format

--- chapter-4/resources/reader-scenario-cleanup.ps1
+++ chapter-4/resources/reader-scenario-cleanup.ps1
@@ -15,24 +15,24 @@ $userid=$(az ad user list --upn $user --query [].objectId -o tsv)
 $managedidentity=$(az ad sp list --display-name LinuxVM --query [].appId -o tsv)
 
 ## Clean up role assignments
-Write-Host -ForegroundColor Green "####################################################################"
+Write-Host -ForegroundColor Green "#######################################"
 Write-Host -ForegroundColor Green "# Cleaning up role assignments #"
-Write-Host -ForegroundColor Green "####################################################################"
+Write-Host -ForegroundColor Green "#######################################"
 az role assignment delete --assignee $customappid --role "Contributor"
 az role assignment delete --assignee $containerappid --role "Contributor"
 az role assignment delete --assignee $userid --role "Reader"
 az role assignment delete --assignee $managedidentity --role "Contributor"
 
-Write-Host -ForegroundColor Green "####################################################################"
+Write-Host -ForegroundColor Green "#######################################"
 Write-Host -ForegroundColor Green "# Cleaning up identity objects #"
-Write-Host -ForegroundColor Green "####################################################################"
+Write-Host -ForegroundColor Green "#######################################"
 az ad app delete --id $customappid
 az ad app delete --id $containerappid
 az ad user delete --id $userid
 
-Write-Host -ForegroundColor Green "####################################################################"
+Write-Host -ForegroundColor Green "#######################################"
 Write-Host -ForegroundColor Green "# Cleaning up resource group #"
-Write-Host -ForegroundColor Green "####################################################################"
+Write-Host -ForegroundColor Green "#######################################"
 az group delete -n $group --yes
 
 Write-Host -ForegroundColor Green "Successfully cleaned up resources!!"
