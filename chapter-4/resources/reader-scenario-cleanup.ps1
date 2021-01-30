e73dc7e45205ebcbf67708db160b7d443ab97338
David Okeyode <asegunlolu@hotmail.com>
2021-01-30 01:43:01 +0000

    modified cleanup text in reader scenario

--- chapter-4/resources/reader-scenario-cleanup.ps1
+++ chapter-4/resources/reader-scenario-cleanup.ps1
@@ -1,5 +1,5 @@
 $starttime = Get-Date
-Write-Host -ForegroundColor Green "Deployment Started $starttime"
+Write-Host -ForegroundColor Green "Cleanup Started $starttime"
 
 ## Set the user and service principal names
 $upnsuffix=$(az ad signed-in-user show --query userPrincipalName --output tsv | sed 's/.*@//')
