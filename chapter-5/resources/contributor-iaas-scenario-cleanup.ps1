0bc066d4e881828985cf99e695cc4675e8b83138
David Okeyode <asegunlolu@hotmail.com>
2021-02-28 21:03:46 +0000

    modified contributor cleanup script

--- chapter-5/resources/contributor-iaas-scenario-cleanup.ps1
+++ chapter-5/resources/contributor-iaas-scenario-cleanup.ps1
@@ -11,20 +11,20 @@ $userid=$(az ad user list --upn $user --query [].objectId -o tsv)
 $managedidentity=$(az ad sp list --display-name winvm01 --query [].appId -o tsv)
 
 ## Clean up role assignments
-Write-Host -ForegroundColor Green "#######################################"
+Write-Host -ForegroundColor Green "#################################"
 Write-Host -ForegroundColor Green "# Cleaning up role assignments #"
-Write-Host -ForegroundColor Green "#######################################"
+Write-Host -ForegroundColor Green "#################################"
 az role assignment delete --assignee $userid --role "Contributor"
 az role assignment delete --assignee $managedidentity --role "Contributor"
 
-Write-Host -ForegroundColor Green "#######################################"
+Write-Host -ForegroundColor Green "#################################"
 Write-Host -ForegroundColor Green "# Cleaning up identity objects #"
-Write-Host -ForegroundColor Green "#######################################"
+Write-Host -ForegroundColor Green "#################################"
 az ad user delete --id $userid
 
-Write-Host -ForegroundColor Green "#######################################"
+Write-Host -ForegroundColor Green "################################"
 Write-Host -ForegroundColor Green "# Cleaning up resource group #"
-Write-Host -ForegroundColor Green "#######################################"
+Write-Host -ForegroundColor Green "################################"
 az group delete -n $group --yes
 rm *.ps1
 rm *.txt
